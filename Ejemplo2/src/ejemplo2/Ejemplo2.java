d026bc374c1507169c565d180c4461558f8846ce
JuanMiguel <juan2416@hotmail.com>
2015-10-02 23:24:23 -0500

    1
    Hice cambio en el primer archivo.

--- Ejemplo2/src/ejemplo2/Ejemplo2.java
+++ Ejemplo2/src/ejemplo2/Ejemplo2.java
@@ -16,6 +16,7 @@ public class Ejemplo2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("HAciendo una prueba del primer cambio en archivos");
     }
     
 }
