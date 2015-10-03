13d30c3d35c04915b236062adfa0a30ab0911aa9
JuanMiguel <juan2416@hotmail.com>
2015-10-02 23:42:08 -0500

    Revert "Ejemplo1"
    Listo

--- Clase1/src/clase1/Clase1.java
+++ Clase1/src/clase1/Clase1.java
@@ -15,7 +15,7 @@ public class Clase1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // xxxxxxx TODO code application logic here
     }
     
 }
